4d1698bc7171b8660477436bd19c0a9b333bde9f
valli22 <davidvacasmiguel@hotmail.com>
2018-02-01 19:52:53 +0100

    proyecto clase

---
+++ Assets/Scripts/AgentTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentTest : MonoBehaviour {
+
+	NavMeshAgent navMeshComponent;
+
+	// Use this for initialization
+	void Start () {
+		navMeshComponent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hitInfo;
+			if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo)) {
+				navMeshComponent.destination = hitInfo.point;
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/Patrulla.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrulla : MonoBehaviour {
+
+	[SerializeField]
+	GameObject points;
+	int totalPoints;
+
+	int actualPoint;
+
+	NavMeshAgent navMeshComponent;
+
+	[SerializeField]
+	GameObject player;
+
+	public float distanceToChase = 3;
+
+	// Use this for initialization
+	void Start () {
+		actualPoint = 0;
+		navMeshComponent = GetComponent<NavMeshAgent>();
+		totalPoints = points.transform.childCount;
+		navMeshComponent.destination = points.transform.GetChild (actualPoint).transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ((player.transform.position - transform.position).magnitude <= distanceToChase) {
+			navMeshComponent.destination = player.transform.position;
+		} else {
+			if (navMeshComponent.remainingDistance <= 0.1) {
+				actualPoint = (actualPoint + 1) % totalPoints;
+				navMeshComponent.destination = points.transform.GetChild (actualPoint).transform.position;
+			}
+		}
+	}
+}
